4d3c057a15b483bd937af2bf673d6e307ed35852
jihadarfat <arfatjoj@gmail.com>
2024-08-25 17:17:25 +0300

    adjusting the polybot endpoint

--- k8s/yolo5/app.py
+++ k8s/yolo5/app.py
@@ -176,7 +176,7 @@ def store_prediction_summary_in_dynamodb(prediction_id, original_img_path, predi
 
 
 def send_get_request_to_polybot(prediction_id, chat_id):
-    polybot_endpoint = 'polybotjihad.devops-int-college.com'
+    polybot_endpoint = 'polybotjihad.jihad.solutions'
     polybot_url = f'https://{polybot_endpoint}/results?predictionId={prediction_id}&chatId={chat_id}'
 
     # Path to the self-signed certificate file
